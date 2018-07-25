3e371039f386dd2c46fafca9fae50b5780bc034d
Nestor <Nestord.cortes@gmail.com>
2018-07-24 21:47:03 -0700

    update

---
+++ Algorithms/BubbleSort.java
@@ -0,0 +1,20 @@
+import java.io.*;
+
+public class BubbleSort{
+	public static void main(String args[]){
+		
+		int numOfNums = 0;
+			
+		File file = new File()
+		BufferedReader br = new BufferedReader(new FileReader("randomNums.txt"));
+		String str;
+		while((str = br.readline())!= null){
+			System.out.println(str);
+			numOfNums++;
+		}
+		int toBeSorted[numOfNums];
+		
+		
+
+	}	
+}
